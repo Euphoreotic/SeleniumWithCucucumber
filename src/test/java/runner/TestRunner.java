ef42fad22b6ec4970473c17485b0055b73cfadc6
Euphoreotic <48033563+Euphoreotic@users.noreply.github.com>
2020-04-24 10:33:04 -0400

    revert

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,24 +1,17 @@
 package runner;
 
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.junit.runner.RunWith;
 
 /**
- * Created by Karthik on 21/09/2019.
+ * Created by Karthik on 10/26/2016.
  */
 
-@CucumberOptions(features = {"src/test/java/features"} , plugin = {"json:target/cucumber.json","html:target/site/cucumber-pretty"},
+//@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/java/features"} , format = {"json:target/cucumber.json","html:target/site/cucumber-pretty"},
         glue = "steps")
-public class TestRunner extends AbstractTestNGCucumberTests {
-
-
-    @Override
-    @DataProvider
-    //@DataProvider (parallel = true) -- For parallel execution support (which is not going to work for our code)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
+public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
