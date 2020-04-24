b686d67e7456d0169c0018453c6b0b48fdd7c225
Euphoreotic <48033563+Euphoreotic@users.noreply.github.com>
2020-04-24 10:34:06 -0400

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -1,24 +1,14 @@
 package steps;
 
 import Base.BaseUtil;
-
-
-import cucumber.api.PickleStepTestStep;
-import cucumber.api.TestCase;
-import gherkin.pickles.PickleStep;
-import io.cucumber.core.api.Scenario;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
- * Created by Karthik on 31/01/2019.
+ * Created by Karthik on 10/17/2016.
  */
-
 public class Hook extends BaseUtil{
 
 
@@ -29,23 +19,12 @@ public class Hook extends BaseUtil{
     }
 
     @Before
-    public void InitializeTest(Scenario scenario) {
-
-
-        scenarioDef = base.features.createNode(scenario.getName());
+    public void InitializeTest() {
 
         System.out.println("Opening the browser : Firefox");
 
-        /*System.setProperty("webdriver.firefox.marionette", "D:\\Libs\\geckodriver.exe");
-        base.Driver = new FirefoxDriver();*/
-
-
-        //Chrome driver
-//        System.setProperty("webdriver.chrome.driver", "/Users/karthikkk/ChromeDriver/chromedriver");
-//        base.Driver = new ChromeDriver();
-
-        System.setProperty("webdriver.chrome.driver", "/Users/karthikkk/ChromeDriver/chromedriver");
-        base.Driver = new ChromeDriver();
+        System.setProperty("webdriver.firefox.marionette", "D:\\libs\\geckodriver.exe");
+        base.Driver = new FirefoxDriver();
     }
 
 
@@ -56,19 +35,6 @@ public class Hook extends BaseUtil{
             System.out.println(scenario.getName());
         }
         System.out.println("Closing the browser : MOCK");
-        base.Driver.quit();
-    }
-
-    @BeforeStep
-    public void BeforeEveryStep(Scenario scenario) {
-        System.out.println("Before every step " + scenario.getId());
-
-        //((PickleStep)((PickleStepTestStep)
-    }
-
-    @AfterStep
-    public void AfterEveryStep(Scenario scenario) throws NoSuchFieldException, IllegalAccessException {
-        //System.out.println("Before every step " + stepTestStep.getId());
     }
 
 }
